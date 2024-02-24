e2d94690f68704b7a08eba0170fdac9ffa8c750a
hasnaerguig1999 <hasnaerguig1999@gmail.com>
2024-02-24 21:55:36 +0100

    KIC-15 Récupérer et afficher la liste des joueurs disponibles.avec styles

--- screens/PlayersScreen.js
+++ screens/PlayersScreen.js
@@ -1,12 +1,37 @@
-import React from 'react';
-import { View, Text} from 'react-native';
-
+import React, { useEffect, useState } from 'react';
+import { View, Text, ScrollView, Image, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchPlayers } from '../redux/Player/playerActions';
+import { styles } from '../styles/PlayersStyles';
 
 export default function PlayersScreen({ navigation }) {
- 
+  const dispatch = useDispatch();
+  const players = useSelector(state => state.players.players);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    dispatch(fetchPlayers());
+  }, [dispatch]);
+
+  const filteredPlayers = players.filter(player => 
+    (`${player.firstname} ${player.lastname}`.toLowerCase().includes(search.toLowerCase()))
+  );
   return (
-    <View>
-      <Text>Players screen</Text>
+    <View style={styles.container}>
+      <TextInput
+        style={styles.searchInput}
+        value={search}
+        onChangeText={setSearch}
+        placeholder="Search players..."
+      />
+      <ScrollView>
+        {filteredPlayers.map(player => (
+          <TouchableOpacity key={player.id} style={styles.playerContainer} onPress={() => navigation.navigate('PlayersDetail', { player: player })}>
+            <Image style={styles.playerImage} source={{ uri: player.image_path }} />
+            <Text style={styles.playerName}>{player.firstname} {player.lastname}</Text>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
     </View>
   );
 }
